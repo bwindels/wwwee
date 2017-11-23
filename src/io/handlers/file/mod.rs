063d379febb6f9afde772d48daa5e9d435fa8eda
Bruno Windels <bruno.windels@gmail.com>
2017-11-23 13:43:05 +0100

    move draft linux async file reading code to handlers module

---
+++ src/io/handlers/file/mod.rs
@@ -0,0 +1,2 @@
+//#[cfg(linux)]
+//mod linux;
--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,6 +1,8 @@
 pub mod token;
 pub mod context;
+pub mod handlers;
 mod handler;
+
 pub use self::handler::*;
 pub use self::context::*;
 pub use self::token::*;
