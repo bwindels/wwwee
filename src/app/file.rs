68aad9493e5acc165addf2535367b2277a652860
Bruno Windels <bruno@windels.cloud>
2018-11-17 14:11:58 +0100

    use new path api in static file handler
    just to make it compile, but not sure this will still be used
    now we have StaticDirectoryHandler. We'll see.

--- src/app/file.rs
+++ src/app/file.rs
@@ -1,20 +1,17 @@
+use std;
 use http;
 use io::sources::file;
-use std;
-use std::io::Write;
 
 pub struct StaticFileHandler<'a> {
-  path: &'a str,
-  content_type: &'a str,
-  download_as: Option<&'a str>
+  root_dir: &'a file::Directory,
+  force_download: bool
 }
 
 impl<'a> StaticFileHandler<'a> {
-  pub fn new(path: &'a str, content_type: &'a str, download_as: Option<&'a str>) -> StaticFileHandler<'a> {
+  pub fn new(root_dir: &'a file::Directory, force_download: bool) -> StaticFileHandler<'a> {
     StaticFileHandler {
-      path,
-      content_type,
-      download_as
+      root_dir,
+      force_download
     }
   }
 }
@@ -22,24 +19,24 @@ impl<'a> StaticFileHandler<'a> {
 //type PathString = StaticString<[u8;512]>;
 
 impl<'a> http::RequestHandler for StaticFileHandler <'a> {
-  fn read_headers(&mut self, _request: &http::Request, res: &http::Responder) -> std::io::Result<Option<http::Response>> {
-    
+  fn read_headers(&mut self, request: &http::Request, res: &http::Responder) -> std::io::Result<Option<http::Response>> {
     //let range = request.headers().content_range;
-    //let path = Path::abs_with_root(self.root_dir, request.uri);
-    //let path = std::path::Path::new("/home/bwindels/dev/wwwee/test_fixtures/aio/small.txt\0");
-    let path = std::path::Path::new(self.path);
-    let reader = file::Reader::open(path, None)?;
+    // TODO: deal with directory paths
+    // print!("{:x?}", request.url());
+    let path = self.root_dir.sub_path(request.url().get(1..).unwrap())?;
+    let reader = file::Reader::open(&path, None)?;
     let mut response = res.respond(http::status::OK)?;
-    //response.set_header(Header::ContentLength(content_length));
-    response.set_header("Content-Type", self.content_type)?;
+    response.set_header("Content-Type", "text/html")?;
     response.set_header_usize("Content-Length", reader.request_size()?)?;
 
-    if let Some(filename) = self.download_as {
-      response.set_header_writer("Content-Disposition", |ref mut value| {
-        write!(value, "attachment; filename={}", filename)
-      })?;
-    }
+    // if let Some(filename) = self.download_as {
+    //   response.set_header_writer("Content-Disposition", |ref mut value| {
+    //     write!(value, "attachment; filename={}", filename)
+    //   })?;
+    // }
 
     response.finish_with_file(reader)
   }
 }
+
+// fn mime_type_from_extension(url: &'a )
