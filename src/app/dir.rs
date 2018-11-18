38bd9583e5fd93deafd068668b6df9904fd70628
Bruno Windels <bruno@windels.cloud>
2018-11-18 20:00:11 +0100

    move file path calculate to own method

--- src/app/dir.rs
+++ src/app/dir.rs
@@ -14,12 +14,9 @@ impl<'a> StaticDirectoryHandler<'a> {
       index_file
     }
   }
-}
 
-impl<'a> http::RequestHandler for StaticDirectoryHandler <'a> {
-  fn read_headers(&mut self, request: &http::Request, res: &http::Responder) -> std::io::Result<Option<http::Response>> {
-    // TODO: use content_range header
-    let relative_url = request.url().get(1..).and_then(|relative_url| {
+  fn file_path(&self, request_url: &'a str) -> std::io::Result<file::RelativePath<'a, 'a>> {
+    let relative_url = request_url.get(1..).and_then(|relative_url| {
       if relative_url.is_empty() {
         None
       } else {
@@ -35,6 +32,14 @@ impl<'a> http::RequestHandler for StaticDirectoryHandler <'a> {
     } else {
       self.root_dir.sub_path(self.index_file)
     }?;
+    Ok(path)
+  }
+}
+
+impl<'a> http::RequestHandler for StaticDirectoryHandler <'a> {
+  fn read_headers(&mut self, request: &http::Request, res: &http::Responder) -> std::io::Result<Option<http::Response>> {
+    // TODO: use content_range header
+    let path = self.file_path(request.url())?;
     let reader = file::Reader::open(&path, None)?;
     let mut response = res.respond(http::status::OK)?;
     let mime_type = http::mime_type::from_path(request.url(), Some(self.index_file));
